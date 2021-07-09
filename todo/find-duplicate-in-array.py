b1572f03b33e81b1d403fe2bdc6abb0bbcf8cf21
Akshita Dixit <akshitadixit.hack@gmail.com>
2021-07-09 19:39:11 +0530

    Add find dupli

--- todo/find-duplicate-in-array.py
+++ todo/find-duplicate-in-array.py
@@ -16,13 +16,25 @@ def find(arr):
         d[x] = 1
     return 0
 
-arr = list(map(int, input().split()))
-print(find(arr))
-
 from collections import Counter
 def find2(arr):
     c = Counter(arr)
     return (c.most_common()[0][0])
 
 def find3(arr):
-    
+    '''
+    since array contains all +ve nos from 1 to n
+    we change the sign of the number at the a[i]th index
+    and if the ith index is already -ve then we have our duplicate a[i]
+    '''
+    for i in range(len(arr)):
+        if arr[abs(arr[i])]<0:
+            print(arr)
+            return abs(arr[abs(arr[i])])
+        arr[arr[i]] *= -1
+
+    return -1
+
+#arr = list(map(int, input().split()))
+ar = [ 1, 5 , 8, 7, 6, 2, 3, 4, 7]
+print(find3(ar))
